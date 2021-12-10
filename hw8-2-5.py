c955131d2ef59860fc5a07b0727c589f09004230
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2021-12-10 01:24:04 -0500

    Update hw8-2-5.py

--- hw8-2-5.py
+++ hw8-2-5.py
@@ -1,5 +1,6 @@
 # Author: JD 12/09/2021
 
+# The version of my understanding
 def weird_sum(li):
     li2 = []
     for x in li:
@@ -18,3 +19,17 @@ print(weird_sum([2, 4, 6, 8, 9]) == 20,
 weird_sum([13, 12, 10]) == 22,
 weird_sum([14, 16, 32]) == 62)
 
+# The version of your explaination in email
+def weird_sum(li):
+    total = 0
+    for x in li:
+        if x % 2 != 0:
+            return total
+        total += x
+        
+    return total
+
+print(weird_sum([2, 4, 6, 8, 9]) == 20,
+weird_sum([13, 12, 10]) == 0,
+weird_sum([14, 16, 32]) == 62)
+        
